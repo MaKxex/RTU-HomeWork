2bfc7485db617c30b3ab863a943906a022535ef2
MaKxex <70351676+MaKxex@users.noreply.github.com>
2021-09-07 20:30:39 +0300

    Add files via upload

--- md1.java
+++ md1.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class md1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float x=0, y=0;
-        System.out.println("TEST");
+        float x = 0, y = 0;
+        System.out.println("211RDB284 Gandžumjans Maksims 2");
         
         System.out.print("x=");
         if (sc.hasNextFloat())
                 x = sc.nextFloat();
         else {
-            System.out.println("input-output error");
-            sc.close();
-            return;
+        System.out.println("input-output error");
+        sc.close();
+        return;
         }
         System.out.print("y=");
         if (sc.hasNextFloat())
@@ -32,12 +32,13 @@ public class md1 {
         float circleB = (x-9)*(x-9)+(y-7)*(y-7);
         float circleC = (x-7)*(x-7)+(y-9)*(y-9);
 
-        if ((circleA <=4 || circleB <=4)  && y >= 7 || circleC <= 4 && y > 9)
+        
+        if ((circleA <=4 || circleB <=4)  && y >= 7 || circleC < 4 && y > 9)
                 System.out.println("green");
         else if (y>=3 && y < 7 && y>= 10 -x && y <= y +3)
                 System.out.println("blue");
             else
-                if (x > 5 && x <9 && y > 7 && y < 9 && !(circleA <=4 && circleB <=4  && y > 7 || circleC <= 4 && y > 9))
+                if (x > 5 && x <9 && y >= 7 && y <= 9 && !(circleA <=4 && circleB <=4  && y > 7 || circleC <= 4 && y > 9))
                         System.out.println("red");
                 else
                         System.out.println("white");
